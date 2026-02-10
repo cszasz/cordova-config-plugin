f4a29bbef4d64b24debcac977800c3454e121b15
MacMini <cszasz@gmail.com>
2026-02-10 09:29:51 +0100

    Update AppConfig.js

--- www/AppConfig.js
+++ www/AppConfig.js
@@ -2,11 +2,6 @@ var exec = require('cordova/exec');
 
 module.exports = {
     getPreference: function (key, success, error) {
-        console.log("AppConfig.getPreference("+key+")");
-        try {
-	   exec(success, error, "AppConfig", "getPreference", [key]);
-	} catch (ex) {
-           error(error)
-	}       
+	exec(success, error, "AppConfig", "getPreference", [key]);
     }
 };
